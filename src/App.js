842369695e8548a97a448806cbc02d01bbab14ce
Divyaranjan Pradhan <divyaranjan619@gmail.com>
2023-12-02 13:58:34 +0530

    Experience Section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import Socials from "./components/Socials";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Home/>
       <About/>
       <Portfolio/>
+      <Experience/>
+      <Contact/>
       <Socials/>
     </div>
   );
